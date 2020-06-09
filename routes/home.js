4cc2f2470e8563afbfa897a20d6ca2c1a2b98dca
Doddger34 <cetin.eray@hotmail.com>
2020-06-09 22:54:36 +0300

    m

--- routes/home.js
+++ routes/home.js
@@ -26,5 +26,10 @@ const router = express.Router();
 //     }
 //   })
 // })
-
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        Name:'Eray',
+        SurName:'Sofi:D'
+    })
+})
 module.exports = router;
