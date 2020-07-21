08b442044aa49a507f6b01fe4550d791c7bbd460
Doddger34 <cetin.eray@hotmail.com>
2020-07-21 08:01:09 -0700

    debug

--- graphql/resolvers/mutation/not.mutation.js
+++ graphql/resolvers/mutation/not.mutation.js
@@ -1,19 +1,10 @@
 module.exports = {
     createNot: async (parent, { data: { Name, Link, lessonId, userId } }, { Not }) => {
-        if (Not.Name === " ") {
-          throw new Error('asd');
-        }
-        if (Not.Link === " ") {
-          throw new Error('asd');
-        }
-        if (Not.lessonId === " ") {
-          throw new Error('asd');
-        }
-        return await new Not({
-            Name,
-            Link,
-            lessonId,
-            userId
-        }).save();
+      return await new Not({
+          Name,
+          Link,
+          lessonId,
+          userId
+      }).save();
     }
 }
